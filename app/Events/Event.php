64b79e8ae86678a459dc7f0e7ba74f7e275d95ec
olivier47adrian <olivier47.adrian@yahoo.fr>
2016-01-26 15:51:54 +0100

    Premier commit

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MyApp\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MyApp\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
